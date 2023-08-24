f732d74cc6eb6209e53e1731b96e90012cbf5b77
Luis Fernando Ramírez Rivas <114302687+Fernando-droidx@users.noreply.github.com>
2023-08-23 22:28:49 -0600

    Subiendo los metosdos: average, midgray, luminosity

--- EscaladeGrises/Form1.Designer.cs
+++ EscaladeGrises/Form1.Designer.cs
@@ -33,13 +33,17 @@
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(46, 99);
+            this.pictureBox1.Location = new System.Drawing.Point(154, 80);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(188, 111);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -50,7 +54,7 @@
             // LBLtitulo
             // 
             this.LBLtitulo.AutoSize = true;
-            this.LBLtitulo.Location = new System.Drawing.Point(222, 34);
+            this.LBLtitulo.Location = new System.Drawing.Point(387, 31);
             this.LBLtitulo.Name = "LBLtitulo";
             this.LBLtitulo.Size = new System.Drawing.Size(92, 15);
             this.LBLtitulo.TabIndex = 1;
@@ -58,9 +62,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(67, 258);
+            this.button1.Location = new System.Drawing.Point(210, 230);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(75, 82);
             this.button1.TabIndex = 2;
             this.button1.Text = "Cargar";
             this.button1.UseVisualStyleBackColor = true;
@@ -68,7 +72,7 @@
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(349, 99);
+            this.pictureBox2.Location = new System.Drawing.Point(508, 62);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(188, 111);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -77,19 +81,39 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(415, 258);
+            this.button2.Location = new System.Drawing.Point(210, 345);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(75, 82);
             this.button2.TabIndex = 4;
             this.button2.Text = "Convertir";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(508, 190);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(188, 111);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(508, 325);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(188, 111);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 6;
+            this.pictureBox4.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(615, 450);
+            this.ClientSize = new System.Drawing.Size(759, 458);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.button1);
@@ -99,6 +123,8 @@
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -111,5 +137,7 @@
         private Button button1;
         private PictureBox pictureBox2;
         private Button button2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
     }
 }
--- EscaladeGrises/Form1.cs
+++ EscaladeGrises/Form1.cs
@@ -5,7 +5,7 @@ namespace EscaladeGrises
 {
     public partial class Form1 : Form
     {
-        Bitmap myPic, final;
+        Bitmap myPic, final1, final2, final3;
        
         public Form1()
         {
@@ -39,21 +39,43 @@ namespace EscaladeGrises
         {
             int w = myPic.Width , h= myPic.Height;
             //Este bucle recorre cada pixel del bitmap
-            Color actual, newC;
-            final = new Bitmap(w, h);
+            Color actual1, actual2, actual3, newC1, newC2, newC3;
+            final1 = new Bitmap(w, h);
+            final2 = new Bitmap(w, h);
+            final3 = new Bitmap(w, h);
             for (int x = 0; x < w; x++)
             {
                 for (int y = 0; y < h; y++)
                 {
-                    actual = myPic.GetPixel(x, y);
-                    newC = Color.FromArgb(actual.R, actual.R, actual.R);
-                    final.SetPixel(x,y,newC);   
+                    //Primer picbox
+                    actual1 = myPic.GetPixel(x, y);
+                    int average = (actual1.R + actual1.G + actual1.B) / 3;
+                    newC1 = Color.FromArgb(average, average, average);
+                    final1.SetPixel(x,y,newC1);
 
+
+                    //Segundo picbox
+                    actual2 = myPic.GetPixel(x, y);
+                    int Hsl = ((actual1.R + actual1.G + actual1.B) / 3);
+                    newC2 = Color.FromArgb(Hsl, Hsl, Hsl);
+                    final2.SetPixel(x, y, newC2);
+
+
+                    //Tercero picbox
+                    actual3 = myPic.GetPixel(x, y);
+                    double  Luminosity = 0.299* (actual3.R) + 0.587*(actual3.G) + 0.114*(actual3.B);
+                    int intValue = (int)Luminosity;//We get a parse 
+                    newC3 = Color.FromArgb(intValue, intValue, intValue);
+                    final3.SetPixel(x, y, newC3);
                 }
 
             }
-            pictureBox2.Image = final;
+            pictureBox2.Image = final1;//primer picture box
+            pictureBox3.Image = final2;
+            pictureBox4.Image = final3;
 
         }
+
+       
     }
 }
